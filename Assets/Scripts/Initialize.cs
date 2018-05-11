7b4ed941b62ea185206ddf4295c5dd1c1b074178
zouyuheng <zouyuheng1998@gmail.com>
2018-05-11 08:57:47 -0700

    Create an object in script

---
+++ Assets/Scripts/Initialize.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialize : MonoBehaviour {
+
+    public GameObject wallPrefab;
+
+	// Use this for initialization
+	void Start () {
+        GameObject wall = Instantiate(wallPrefab);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
