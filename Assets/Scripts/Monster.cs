adece965a4cc8730fb0e8446befb2cd0bd24dfc0
davidlee1999 <1360230697@qq.com>
2018-05-13 01:45:44 +0800

    Monster with bugs

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+    // Use this for initialization
+    public Player player;
+    public double speed;
+    public Vector3 dir, player_location, now_location;
+	void Start () {
+        speed = 0.5;
+	}
+	
+    void Rotate_Face()
+    {
+        transform.localRotation = Quaternion.LookRotation(dir);
+        transform.localEulerAngles.Set(transform.localEulerAngles.x, transform.localEulerAngles.y - 150.0f, transform.localEulerAngles.z);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        // Check if Monster can see Player, if so, change direction to player
+        now_location = transform.localPosition;
+        player_location = player.transform.localPosition;
+        Ray ray_to_player = new Ray(now_location, player_location);
+        RaycastHit hit_info = new RaycastHit();
+        int layer_mark = (1 << 9) | (1 << 10); // only hit wall or player
+        Physics.Raycast(ray_to_player, out hit_info, 1e9f, layer_mark);
+        dir = player_location - now_location;
+        //if (hit_info.collider.CompareTag("Player") || true)
+            //dir = player_location - now_location;
+
+        Rotate_Face();
+    }
+}
