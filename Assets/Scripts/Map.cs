88f3eb7ba84b4bdc01ae46a2af19ac7e541bed31
liangjs <761232680@qq.com>
2018-05-12 15:37:53 +0800

    fix compile error in Map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,18 +1,7 @@
 using System;
 namespace Pacman3D
 {
-    public const int Empty = 0;
-    public const int Wall = 1;
-    public const int Player = 2;
-    public const int Bean = 3;
-    public const int Monster = 4;
-    public const int Obstacle = 5;
-    public const float rate = 1.0; // Game / World
-    public const float xBias = 0.0;
-    public const float yBias = 0.0;
-    public const int Monster_Num = 5;
-	
-	class GamePos
+    public class GamePos
 	{
 		public int x;
 		public int y;
@@ -20,7 +9,7 @@ namespace Pacman3D
             x = _x;
             y = _y;
         }
-        public GamePos(ref Point3D p)
+        public GamePos(Point3D p)
         {
             x = (int)p.x;
             y = (int)p.y;
@@ -28,9 +17,19 @@ namespace Pacman3D
 	}
 	
 
-	class GameMap
-	{
-		public int n,m;
+	public class GameMap
+    {
+        public const int Empty = 0;
+        public const int Wall = 1;
+        public const int Player = 2;
+        public const int Bean = 3;
+        public const int Monster = 4;
+        public const int Obstacle = 5;
+        public const float rate = 1.0f; // Game / World
+        public const float x_bias = 0.0f;
+        public const float y_bias = 0.0f;
+        public const int Monster_Num = 2;
+        public int n,m;
         public int[,] t;
 		public GameMap(int _n = 0, int _m = 0)
         {
@@ -41,94 +40,95 @@ namespace Pacman3D
             {
                 for (int j = 0; j < m; ++j)
                 {
-                    t[i][j] = Empty;
+                    t[i,j] = Empty;
                 }
             }
         }
 		
 		
-		public GamePos worldToGame(ref Point3D worldPos)
+		public GamePos worldToGame(Point3D worldPos)
         {
             float x = worldPos.x, y = worldPos.y;
             x = x * rate + x_bias;
             y = y * rate + y_bias;
-            return GamePos((int) x , (int) y);
+            return new GamePos((int) x , (int) y);
         }
 		
-		public Point3D gameToWorld(ref GamePos gamePos, float height)
+		public Point3D gameToWorld(GamePos gamePos, float height)
         {
-            float x = worldPos.x, y = worldPos.y;
+            float x = gamePos.x, y = gamePos.y;
             x = (x - x_bias) / rate;
             y = (y - y_bias) / rate;
-            return Point3D(x, y, height);
+            return new Point3D(x, y, height);
         }
-		
-		private void buildWallUp(int x, int y)
+
+        private void setType(GamePos p, int _t)
         {
-            setType(GamePos(x, y), Wall);
-            setType(GamePos(x - 1, y), Wall);
-            setType(GamePos(x - 2, y), Wall);
+            t[p.x, p.y] = _t;
+        }
+        private void buildWallUp(int x, int y)
+        {
+            setType(new GamePos(x, y), Wall);
+            setType(new GamePos(x - 1, y), Wall);
+            setType(new GamePos(x - 2, y), Wall);
 
         }
         private void buildWallLeft(int x, int y)
         {
-            setType(GamePos(x, y), Wall);
-            setType(GamePos(x, y - 1), Wall);
-            setType(GamePos(x, y - 2), Wall);
+            setType(new GamePos(x, y), Wall);
+            setType(new GamePos(x, y - 1), Wall);
+            setType(new GamePos(x, y - 2), Wall);
         }
-        private Point3D pointShadow(ref Point3D worldPos){
+        private Point3D pointShadow(Point3D worldPos) {
             float x = worldPos.x, y = worldPos.y;
             x = x* rate + x_bias;
 		    y = y* rate + y_bias;
-		    return Point3D(x, y, 0.0);
+		    return new Point3D(x, y, 0.0);
         }
-        private float cross(ref Point3D a, ref Point3D b){
+        private float cross(Point3D a, Point3D b){
 	    	return a.x* b.y - a.y* b.x;
         }
 
-        private bool inTriangle(ref Point3D p, ref Triangle t){
+        private bool inTriangle(Point3D p, Triangle t){
             Point3D p0 = pointShadow(t.points[0]), p1 = pointShadow(t.points[1]), p2 = pointShadow(t.points[2]);
-            float Sabc = fabs(cross(p1 - p0, p2 - p0));
-            float Spab = fabs(cross(p0 - p, p1 - p));
-            float Spac = fabs(cross(p0 - p, p2 - p));
-            float Spbc = fabs(cross(p1 - p, p2 - p));
-            return fabs(Sabc - Spab - Spac - Spbc) < EPS;
+            float Sabc = System.Math.Abs(cross(p1 - p0, p2 - p0));
+            float Spab = System.Math.Abs(cross(p0 - p, p1 - p));
+            float Spac = System.Math.Abs(cross(p0 - p, p2 - p));
+            float Spbc = System.Math.Abs(cross(p1 - p, p2 - p));
+            return System.Math.Abs(Sabc - Spab - Spac - Spbc) < EPS;
         }
 		
-		private void setType(ref GamePos p, int _t){
-            t[p.x][p.y] = _t;
-	    }
-        private void addTriangle(ref Triangle T)
+        private void addTriangle(Triangle T)
         {
             for (int i = 0; i < n; ++i)
             {
                 for (int j = 0; j < m; ++j)
                 {
-                    if (t[i][j] == Obstacle) continue;
+                    if (t[i,j] == Obstacle) continue;
 
                     float x = (float)i, y = (float)j;
                     Point3D p = new Point3D(x + EPS, y + EPS);
                     if (inTriangle(p, T))
                     {
-                        setType(GamePos(i, j), Obstacle);
+                        setType(new GamePos(i, j), Obstacle);
                         continue;
                     }
                     p = new Point3D(x + 1 - EPS, y + 1 - EPS);
                     if (inTriangle(p, T))
                     {
-                        setType(GamePos(i, j), Obstacle);
+                        setType(new GamePos(i, j), Obstacle);
                         continue;
                     }
                     p = new Point3D(x + 1 - EPS, y + EPS);
                     if (inTriangle(p, T))
                     {
-                        setType(GamePos(i, j), Obstacle);
+                        setType(new GamePos(i, j), Obstacle);
                         continue;
                     }
                     p = new Point3D(x + EPS, y + 1 - EPS);
                     if (inTriangle(p, T))
                     {
-                        setType(GamePos(i, j), Obstacle);
+                        setType(new GamePos(i, j), Obstacle);
                         continue;
                     }
                 }
@@ -142,20 +142,20 @@ namespace Pacman3D
             for (int i = 0; i < n; ++i)
                 for (int j = 0; j < m; ++j)
                 {
-                    if (t[i][j] == Empty) g[i][j] = 0;
-                    else g[i][j] = 1; //obstacle
+                    if (t[i,j] == Empty) g[i,j] = 0;
+                    else g[i,j] = 1; //obstacle
                 }
             int cnt = 2, k;
             for (int i = 3; i < n; i += 3)
             {
                 for (int j = 3; j < m; j += 3)
                 {
-                    if (t[i][j] != Empty) continue;
+                    if (t[i,j] != Empty) continue;
 
 
-                    if (g[i - 3][j] != 0 && g[i][j - 3] != 0)
+                    if (g[i - 3,j] != 0 && g[i,j - 3] != 0)
                     {
-                        if (g[i - 3][j] == g[i][j - 3])
+                        if (g[i - 3,j] == g[i,j - 3])
                         {
                             k = ran.Next(0, 99);
                             if (k < 60)
@@ -163,12 +163,12 @@ namespace Pacman3D
                                 k = ran.Next(0, 1);
                                 if (k == 0)
                                 {
-                                    g[i][j] = g[i - 3][j];
+                                    g[i,j] = g[i - 3,j];
                                     buildWallUp(i, j);
                                 }
                                 else
                                 {
-                                    g[i][j] = g[i][j - 3];
+                                    g[i,j] = g[i,j - 3];
                                     buildWallLeft(i, j);
                                 }
                             }
@@ -178,60 +178,60 @@ namespace Pacman3D
                             k = ran.Next(0, 119);
                             if (k < 30)
                             {
-                                g[i][j] = g[i - 3][j];
+                                g[i,j] = g[i - 3,j];
                                 buildWallUp(i, j);
                             }
                             else if (k < 60)
                             {
-                                g[i][j] = g[i][j - 3];
+                                g[i,j] = g[i,j - 3];
                                 buildWallLeft(i, j);
                             }
                             else if (k < 80)
                             {
-                                int clr = g[i - 3][j];
+                                int clr = g[i - 3,j];
                                 for (int _i = 0; _i < n; ++_i)
                                 {
                                     for (int _j = 0; _j < m; ++_j)
                                     {
-                                        if (g[_i][_j] == clr)
+                                        if (g[_i,_j] == clr)
                                         {
-                                            g[_i][_j] = g[i][j - 3];
+                                            g[_i,_j] = g[i,j - 3];
                                         }
                                     }
                                 }
-                                g[i][j] = g[i][j - 3];
+                                g[i,j] = g[i,j - 3];
                                 buildWallUp(i, j);
                                 buildWallLeft(i, j);
                             }
                         }
                     }
-                    else if (g[i - 3][j] != 0)
+                    else if (g[i - 3,j] != 0)
                     { //only up
                         k = ran.Next(0, 89);
                         if (k < 30)
                         {
-                            g[i][j] = g[i - 3][j];
+                            g[i,j] = g[i - 3,j];
                             buildWallUp(i, j);
                         }
                         else if (k < 60)
                         {
-                            g[i][j] = g[i][j - 3] = cnt++;
-                            setType(GamePos(i, j - 3), Wall);
+                            g[i,j] = g[i,j - 3] = cnt++;
+                            setType(new GamePos(i, j - 3), Wall);
                             buildWallLeft(i, j);
                         }
                     }
-                    else if (g[i][j - 3] != 0)
+                    else if (g[i,j - 3] != 0)
                     { //only left
                         k = ran.Next(0, 89);
                         if (k < 30)
                         {
-                            g[i][j] = g[i - 3][j] = cnt++;
-                            setType(GamePos(i - 3, j), Wall);
+                            g[i,j] = g[i - 3,j] = cnt++;
+                            setType(new GamePos(i - 3, j), Wall);
                             buildWallUp(i, j);
                         }
                         else if (k < 60)
                         {
-                            g[i][j] = g[i][j - 3];
+                            g[i,j] = g[i,j - 3];
                             buildWallLeft(i, j);
                         }
                     }
@@ -240,21 +240,21 @@ namespace Pacman3D
                         k = ran.Next(0, 119);
                         if (k < 30)
                         {
-                            g[i][j] = g[i - 3][j] = cnt++;
-                            setType(GamePos(i - 3, j), Wall);
+                            g[i,j] = g[i - 3,j] = cnt++;
+                            setType(new GamePos(i - 3, j), Wall);
                             buildWallUp(i, j);
                         }
                         else if (k < 60)
                         {
-                            g[i][j] = g[i][j - 3] = cnt++;
-                            setType(GamePos(i, j - 3), Wall);
+                            g[i,j] = g[i,j - 3] = cnt++;
+                            setType(new GamePos(i, j - 3), Wall);
                             buildWallLeft(i, j);
                         }
                         else if (k < 90)
                         {
-                            g[i][j] = g[i - 3][j] = g[i][j - 3] = cnt++;
-                            setType(GamePos(i - 3, j), Wall);
-                            setType(GamePos(i, j - 3), Wall);
+                            g[i,j] = g[i - 3,j] = g[i,j - 3] = cnt++;
+                            setType(new GamePos(i - 3, j), Wall);
+                            setType(new GamePos(i, j - 3), Wall);
                             buildWallUp(i, j);
                             buildWallLeft(i, j);
                         }
@@ -264,22 +264,23 @@ namespace Pacman3D
             }
 
         }
-        public bool detectPlayer(ref Monster m, int direction)
+        /*
+        public bool detectPlayer(Monster m, int direction)
         {
             if (direction == left)
             {
                 for (int i = m.p.y; i >= 0; --i)
                 {
-                    if (t[m.p.x][i] == Wall) break;
-                    else if (t[m.p.x][i] == Player) return true;
+                    if (t[m.p.x,i] == Wall) break;
+                    else if (t[m.p.x,i] == Player) return true;
                 }
             }
             else if (direction == right)
             {
                 for (int i = m.p.y; i < m; ++i)
                 {
-                    if (t[m.p.x][i] == Wall) break;
-                    else if (t[m.p.x][i] == Player) return true;
+                    if (t[m.p.x,i] == Wall) break;
+                    else if (t[m.p.x,i] == Player) return true;
                 }
             }
             else if (direction == up)
@@ -298,26 +299,27 @@ namespace Pacman3D
                     else if (t[i][m.p.y] == Player) return true;
                 }
             }
-        }
+            return false;
+        }*/
     };
-	
-	class Monster
+
+    public class Monster
 	{
 		public Circle cir;
         public GamePos p;
-		Monster(ref Circle _p){
+		public Monster(Circle _p){
             cir = _p;
             p = new GamePos(_p.c);
         }
 
-        public void move(ref Point3D _p)
+        public void move(Point3D _p)
         {
             cir.c = cir.c + _p;
-            p = GamePos(pos);
+            p = new GamePos(cir);
         }
 	};
 
-	public class SuccesiveGameMap:GameMap 
+	public class SuccesiveGameMap : GameMap 
 	{
 		Rectangle [] Recs;
 		Circle [] Beans;
@@ -340,11 +342,11 @@ namespace Pacman3D
 	    private void generateBeans(int bnum)
         {
             Random ran = new Random();
-            BeanNum = bnum;
-            for (int i = 0; i < BeanNum; )
+            //BeanNum = bnum;
+            for (int i = 0; BeanNum < bnum && i < n * m * 10 ; i++)
             {
-                float x = ran.NextDouble() * xLimit;
-                float y = ran.NextDouble() * yLimit;
+                float x = (float)ran.NextDouble() * xLimit;
+                float y = (float)ran.NextDouble() * yLimit;
                 Circle tmp = new Circle(x, y);
                 bool flag = true;
                 for (int j = 0; j < RecNum; ++j)
@@ -362,17 +364,20 @@ namespace Pacman3D
                         flag = false;
                     }
                 }
-                if (flag) Beans[i++] = tmp;
+                if (flag)
+                {
+                    Beans[BeanNum++] = tmp;
+                }
             }
         }
         void generateMonsters()
         {
             Random ran = new Random();
-            for (int i = 0; i < 3;)
+            for (int i = 0; i < Monster_Num;)
             {
-                float x = ran.NextDouble() * xLimit;
-                float y = ran.NextDouble() * yLimit;
-                Monster tmp = new Monster(Circle(x, y));
+                float x = (float)ran.NextDouble() * xLimit;
+                float y = (float)ran.NextDouble() * yLimit;
+                Monster tmp = new Monster(new Circle(x, y));
                 bool flag = true;
                 for (int j = 0; j < RecNum; ++j)
                 {
@@ -393,12 +398,13 @@ namespace Pacman3D
         void generateMap()
         {// using base's generate and transform to successive
             generate();
-            generateBeans();
+            generateBeans(n * m);
             generateMonsters();
         }
 		// need : generate() -> generateBeans() -> generateMonsters() 
 		
-		void paintMap(); //generate a graph for small map
+		//void paintMap(); //generate a graph for small map
+       
 		/* @myp : fill this ! */
 	};
 }
