237eba1ba4ace0ddc5c2e99395c7ac87d89e104b
davidlee1999 <1360230697@qq.com>
2018-05-12 20:26:05 +0800

    hit wall

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,16 @@ public class Player : MonoBehaviour {
 
     public GameObject mixedRealityCamera;
     public Text countText, winText;
-    public int count;
+    public int count, hit_wall;
+    public double hit_wall_time;
+    public string status;
     // Use this for initialization
     void Start () {
         count = 0;
+        hit_wall = 0;
         SetCountText();
         winText.text = "";
-
+        status = "live";
     }
 	
 	// Update is called once per frame
@@ -21,34 +24,72 @@ public class Player : MonoBehaviour {
         Transform cameraTransform = mixedRealityCamera.GetComponent<Transform>();
         Vector3 position = cameraTransform.localPosition;
         transform.localPosition = position;
+
+        SetCountText();
 	}
 
     // When this game object intersects a collider with 'is trigger' checked, 
     // store a reference to that collider in a variable named 'other'..
     void OnTriggerEnter(Collider other)
     {
-        // ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
         if (other.gameObject.CompareTag("Bean"))
         {
-            // Make the other game object (the pick up) inactive, to make it disappear
+            // Make the other game object (the Bean) inactive, to make it disappear
             other.gameObject.SetActive(false);
 
             // Add one to the score variable 'count'
             count = count + 1;
 
-            // Run the 'SetCountText()' function (see below)
-            SetCountText();
+            //TODO: add some audio
         }
+
         if (other.gameObject.CompareTag("Monster"))
         {
-            // Run the 'SetCountText()' function (see below)
             count = 0;
-            SetCountText("dead");
+            status = "dead";
+            //TODO: add some audio
         }
+
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            if (status == "live")
+            {
+                status = "wall";
+                hit_wall++;
+                hit_wall_time = 0;
+                if (hit_wall == 3)
+                    status = "dead";
+            }
+            //TODO: add some audio
+        }
+        SetCountText();
     }
 
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            hit_wall_time += Time.deltaTime;
+            if(hit_wall_time > 3.0)
+            {
+                status = "dead";
+            }
+        }
+        SetCountText();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            if (status == "wall")
+                status = "live";
+            hit_wall_time = 0;
+        }
+        SetCountText();
+    }
     // Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
-    void SetCountText(string status = "live")
+    void SetCountText()
     {
         // Update the text field of our 'countText' variable
         countText.text = "Count: " + count.ToString();
@@ -57,12 +98,26 @@ public class Player : MonoBehaviour {
         if (status == "dead")
         {
             winText.text = "Game Over! Please Restart!";
+
+            //TODO: game over
         }
+
+        else if (status == "wall")
+        {
+            winText.text = "Caution: You hit the wall " + hit_wall + "times!";
+
+            //TODO: game over
+        }
+        else if (status == "live")
+            winText.text = "";
+
         // Check if our 'count' is equal to or exceeded 2
         if (count >= 2)
         {
             // Set the text value of our 'winText'
             winText.text = "You Win!";
+
+            //TODO: game over
         }
     }
 }
