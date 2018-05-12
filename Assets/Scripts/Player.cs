0ec67f8d8ad07b638cf4a5a3170c57817a873ee7
zouyuheng <zouyuheng1998@gmail.com>
2018-05-11 20:36:08 -0700

    Collision detection

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject mixedRealityCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform cameraTransform = mixedRealityCamera.GetComponent<Transform>();
+        Vector3 position = cameraTransform.localPosition;
+        transform.localPosition = position;
+	}
+}
