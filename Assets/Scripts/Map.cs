d53cae238d577e75c0dd5a857f685ad6941aef3a
sunyuqi148 <sun_yq@pku.edu.cn>
2018-05-12 11:53:06 +0800

    Map ver 1.0
    Map ver 1.0(with bugs)

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,375 @@
+using System;
+namespace Pacman3D
+{
+    public const int Empty = 0;
+    public const int Wall = 1;
+    public const int Player = 2;
+    public const int Bean = 3;
+    public const int Monster = 4;
+    public const int Obstacle = 5;
+    public const float rate = 1.0; // Game / World
+    public const float xBias = 0.0;
+    public const float yBias = 0.0;
+    public const int Monster_Num = 5;
+	
+	class GamePos
+	{
+		public int x;
+		public int y;
+		public GamePos(int _x = 0, int _y = 0) {
+            x = _x;
+            y = _y;
+        }
+        public GamePos(ref Point3D p)
+        {
+            x = (int)p.x;
+            y = (int)p.y;
+        }
+	}
+	
+
+	class GameMap
+	{
+		public int n,m;
+        public int[,] t;
+		public GameMap(int _n = 0, int _m = 0)
+        {
+            n = _n;
+            m = _m;
+            t = new int[n, m];
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    t[i][j] = Empty;
+                }
+            }
+        }
+		
+		
+		public GamePos worldToGame(ref Point3D worldPos)
+        {
+            float x = worldPos.x, y = worldPos.y;
+            x = x * rate + x_bias;
+            y = y * rate + y_bias;
+            return GamePos((int) x , (int) y);
+        }
+		
+		public Point3D gameToWorld(ref GamePos gamePos, float height)
+        {
+            float x = worldPos.x, y = worldPos.y;
+            x = (x - x_bias) / rate;
+            y = (y - y_bias) / rate;
+            return Point3D(x, y, height);
+        }
+		
+		private void buildWallUp(int x, int y)
+        {
+            setType(GamePos(x, y), Wall);
+            setType(GamePos(x - 1, y), Wall);
+            setType(GamePos(x - 2, y), Wall);
+
+        }
+        private void buildWallLeft(int x, int y)
+        {
+            setType(GamePos(x, y), Wall);
+            setType(GamePos(x, y - 1), Wall);
+            setType(GamePos(x, y - 2), Wall);
+        }
+        private Point3D pointShadow(ref Point3D worldPos){
+            float x = worldPos.x, y = worldPos.y;
+            x = x* rate + x_bias;
+		    y = y* rate + y_bias;
+		    return Point3D(x, y, 0.0);
+        }
+        private float cross(ref Point3D a, ref Point3D b){
+	    	return a.x* b.y - a.y* b.x;
+        }
+
+        private bool inTriangle(ref Point3D p, ref Triangle t){
+            Point3D p0 = pointShadow(t.points[0]), p1 = pointShadow(t.points[1]), p2 = pointShadow(t.points[2]);
+            float Sabc = fabs(cross(p1 - p0, p2 - p0));
+            float Spab = fabs(cross(p0 - p, p1 - p));
+            float Spac = fabs(cross(p0 - p, p2 - p));
+            float Spbc = fabs(cross(p1 - p, p2 - p));
+            return fabs(Sabc - Spab - Spac - Spbc) < EPS;
+        }
+		
+		private void setType(ref GamePos p, int _t){
+            t[p.x][p.y] = _t;
+	    }
+        private void addTriangle(ref Triangle T)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    if (t[i][j] == Obstacle) continue;
+
+                    float x = (float)i, y = (float)j;
+                    Point3D p = new Point3D(x + EPS, y + EPS);
+                    if (inTriangle(p, T))
+                    {
+                        setType(GamePos(i, j), Obstacle);
+                        continue;
+                    }
+                    p = new Point3D(x + 1 - EPS, y + 1 - EPS);
+                    if (inTriangle(p, T))
+                    {
+                        setType(GamePos(i, j), Obstacle);
+                        continue;
+                    }
+                    p = new Point3D(x + 1 - EPS, y + EPS);
+                    if (inTriangle(p, T))
+                    {
+                        setType(GamePos(i, j), Obstacle);
+                        continue;
+                    }
+                    p = new Point3D(x + EPS, y + 1 - EPS);
+                    if (inTriangle(p, T))
+                    {
+                        setType(GamePos(i, j), Obstacle);
+                        continue;
+                    }
+                }
+            }
+        }
+		public void generate() //generate a map 
+        {
+            Random ran = new Random();
+
+            int[,] g = new int[n, m];
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < m; ++j)
+                {
+                    if (t[i][j] == Empty) g[i][j] = 0;
+                    else g[i][j] = 1; //obstacle
+                }
+            int cnt = 2, k;
+            for (int i = 3; i < n; i += 3)
+            {
+                for (int j = 3; j < m; j += 3)
+                {
+                    if (t[i][j] != Empty) continue;
+
+
+                    if (g[i - 3][j] != 0 && g[i][j - 3] != 0)
+                    {
+                        if (g[i - 3][j] == g[i][j - 3])
+                        {
+                            k = ran.Next(0, 99);
+                            if (k < 60)
+                            {
+                                k = ran.Next(0, 1);
+                                if (k == 0)
+                                {
+                                    g[i][j] = g[i - 3][j];
+                                    buildWallUp(i, j);
+                                }
+                                else
+                                {
+                                    g[i][j] = g[i][j - 3];
+                                    buildWallLeft(i, j);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            k = ran.Next(0, 119);
+                            if (k < 30)
+                            {
+                                g[i][j] = g[i - 3][j];
+                                buildWallUp(i, j);
+                            }
+                            else if (k < 60)
+                            {
+                                g[i][j] = g[i][j - 3];
+                                buildWallLeft(i, j);
+                            }
+                            else if (k < 80)
+                            {
+                                int clr = g[i - 3][j];
+                                for (int _i = 0; _i < n; ++_i)
+                                {
+                                    for (int _j = 0; _j < m; ++_j)
+                                    {
+                                        if (g[_i][_j] == clr)
+                                        {
+                                            g[_i][_j] = g[i][j - 3];
+                                        }
+                                    }
+                                }
+                                g[i][j] = g[i][j - 3];
+                                buildWallUp(i, j);
+                                buildWallLeft(i, j);
+                            }
+                        }
+                    }
+                    else if (g[i - 3][j] != 0)
+                    { //only up
+                        k = ran.Next(0, 89);
+                        if (k < 30)
+                        {
+                            g[i][j] = g[i - 3][j];
+                            buildWallUp(i, j);
+                        }
+                        else if (k < 60)
+                        {
+                            g[i][j] = g[i][j - 3] = cnt++;
+                            setType(GamePos(i, j - 3), Wall);
+                            buildWallLeft(i, j);
+                        }
+                    }
+                    else if (g[i][j - 3] != 0)
+                    { //only left
+                        k = ran.Next(0, 89);
+                        if (k < 30)
+                        {
+                            g[i][j] = g[i - 3][j] = cnt++;
+                            setType(GamePos(i - 3, j), Wall);
+                            buildWallUp(i, j);
+                        }
+                        else if (k < 60)
+                        {
+                            g[i][j] = g[i][j - 3];
+                            buildWallLeft(i, j);
+                        }
+                    }
+                    else
+                    {
+                        k = ran.Next(0, 119);
+                        if (k < 30)
+                        {
+                            g[i][j] = g[i - 3][j] = cnt++;
+                            setType(GamePos(i - 3, j), Wall);
+                            buildWallUp(i, j);
+                        }
+                        else if (k < 60)
+                        {
+                            g[i][j] = g[i][j - 3] = cnt++;
+                            setType(GamePos(i, j - 3), Wall);
+                            buildWallLeft(i, j);
+                        }
+                        else if (k < 90)
+                        {
+                            g[i][j] = g[i - 3][j] = g[i][j - 3] = cnt++;
+                            setType(GamePos(i - 3, j), Wall);
+                            setType(GamePos(i, j - 3), Wall);
+                            buildWallUp(i, j);
+                            buildWallLeft(i, j);
+                        }
+                    }
+
+                }
+            }
+
+        }
+        public bool detectPlayer(ref Monster m, int direction)
+        {
+            if (direction == left)
+            {
+                for (int i = m.p.y; i >= 0; --i)
+                {
+                    if (t[m.p.x][i] == Wall) break;
+                    else if (t[m.p.x][i] == Player) return true;
+                }
+            }
+            else if (direction == right)
+            {
+                for (int i = m.p.y; i < m; ++i)
+                {
+                    if (t[m.p.x][i] == Wall) break;
+                    else if (t[m.p.x][i] == Player) return true;
+                }
+            }
+            else if (direction == up)
+            {
+                for (int i = m.p.x; i >= 0; --i)
+                {
+                    if (t[i][m.p.y] == Wall) break;
+                    else if (t[i][m.p.y] == Player) return true;
+                }
+            }
+            else if (direction == down)
+            {
+                for (int i = m.p.x; i < n; ++i)
+                {
+                    if (t[i][m.p.y] == Wall) break;
+                    else if (t[i][m.p.y] == Player) return true;
+                }
+            }
+        }
+    };
+	
+	class Monster
+	{
+		public Point3D pos;
+        public GamePos p;
+		Monster(ref Point3D _p){
+            pos = _p;
+            p = new GamePos(_p);
+        }
+
+        public void move(ref Point3D _p)
+        {
+            pos = pos + _p;
+            p = GamePos(pos);
+        }
+	};
+
+	public class SuccesiveGameMap:GameMap 
+	{
+		Rectangle [] Recs;
+		Circle [] Cirs;
+        private const int CirMaxNum = 100;
+        public int CirNum;
+        public int RecNum;
+        public float xLimit;
+        public float yLimit;
+        public int BeanNum;
+		SuccesiveGameMap(int _n = 0, int _m = 0):base(_n, _m)
+        {
+            Recs = new Rectangle[_n * _m];
+            Cirs = new Circle[CirNum];
+            xLimit = (float)_n - EPS;
+            yLimit = (float)_m - EPS;
+        }
+		
+	    private void generateBeans(int bnum)
+        {
+            Random ran = new Random();
+            BeanNum = bnum;
+            for (int i = 0; i < BeanNum; )
+            {
+                float x = ran.NextDouble() * xLimit;
+                float y = ran.NextDouble() * yLimit;
+                Circle tmp = new Circle()
+                for (int j = 0; j < RecNum; ++j)
+                {
+                    
+                }
+            }
+        }
+        void generateMonsters();
+		
+	public:	
+		void setPlayer(const GamePos & p);
+		void generateMap(); // using base's generate and transform to successive
+		// need : generate() -> generateBeans() -> generateMonsters() 
+		
+		void paintMap(); //generate a graph for small map
+		/* @myp : fill this ! */
+	};
+
+	struct GameBoard
+	{
+		GameMap gameMap;
+		GamePos playerPos;
+		int monsterNum;
+		std::vector<Monster> monsters;
+
+		/* other necessary properties and functions */
+	};
+
+	GameMap *generateMap(const std::vector<Triangle> &mesh);
+}
