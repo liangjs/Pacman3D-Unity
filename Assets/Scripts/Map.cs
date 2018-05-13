6d719ffe59f63ec9d49be08ea6c0f32f4fc39a28
zouyuheng <zouyuheng1998@gmail.com>
2018-05-12 20:09:07 -0700

    Monster walking when collide with wall

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -518,7 +518,7 @@ namespace Pacman3D
             xLimit = (float)_n - FloatCmp.EPS;
             yLimit = (float)_m - FloatCmp.EPS;
         }
-}
+
 
         private void generateBeans(int bnum)
         {
