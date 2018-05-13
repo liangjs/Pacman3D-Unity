a0e0a563031ba9ae96683488402574d4fc48e6a0
davidlee1999 <1360230697@qq.com>
2018-05-13 10:33:11 +0800

    not easy to crash wall

---
+++ Assets/Scripts/planeHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeHelper : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(obj);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+}
